a93ce1e661c652f9d70b949c1d4ecd8e1d8fc15e
Ilja-Prihach <piratehnik9508@gmail.com>
2025-09-05 00:58:25 +0300

    HW-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const newState = [...state];
+            if (action.payload === 'up') {
+                return newState.sort((a, b) => a.name.localeCompare(b.name));
+            }
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return newState.sort((a, b) => b.name.localeCompare(a.name));
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
